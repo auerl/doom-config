127cf8d654954ec37ff15361878615191c39f343
Ludwig Auer <ludwig.auer@gmail.com>
2019-06-30 23:37:07 +0200

    install additional packages

---
+++ packages.el
@@ -0,0 +1,11 @@
+;; -*- no-byte-compile: t; -*-
+;;; ~/.doom.d/packages.el
+
+(package! cmake-ide)
+(package! move-text)
+(package! auto-complete-clang)
+(package! irony)
+(package! rtags)
+(package! flycheck)
+(package! helm-swoop)
+(package! elpy)
